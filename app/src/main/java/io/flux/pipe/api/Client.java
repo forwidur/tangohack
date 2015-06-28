1bb5ad365b05e83c79ee854d86aef1022d97e2c0
Max Grigorev <mag@flux.io>
2015-06-27 20:50:46 -0700

    Changes keys and Tango as a client.

--- app/src/main/java/io/flux/pipe/api/Client.java
+++ app/src/main/java/io/flux/pipe/api/Client.java
@@ -12,8 +12,9 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Client {
+    private static final String key = "40cf35e505c3868d1215a87de246da42";
     private static final String projectsUrl = "https://polson.flux.io/p/aroJ3KnpdK8rrYkJO/api/"
-            + "value/key/v/408666cea264b2b39e07fec8214b6d5c";
+            + "value/key/v/";
     private static final String auth_ = "auth=MTQzNTQ0ODQ5N3xfN09yNngxRFJRSVNHejF5ZDI0TDF2NXo0XzdJc"
             + "kxlV3dLT3huMXAtTVhHdlpFekNraDFTakFfMmxjUWRUalJNSk04RFc5TWEzX0kxTkhrUlp3WEt0SE9NZ0gya"
             + "kY2dkJDQy15ZDlsY0QySzZqbUlVRTJ4Wmd6alFQdEVmRlBFMHUzd1NUTXJlY1ZVckM5dkVxTzJ6MlUtMUpWb"
@@ -21,7 +22,7 @@ public class Client {
             + "GR0dl80SURPTENPODhLd2J6YUtZMldBcmRhN2RZUkNnWmx3YXNBSjRzWWdnWmw2N0ViZ1p2Nko3blRTTE51S"
             + "kJXMzBKSXVyQmRTWnlZc091TVU0azhKUmFnLUU1RklyYm9vUWNqeUNjN2szeWFwaTI0VnVDSmNibjBzdlJqc"
             + "W1uM2F8QerFYL-vXba0gpnDZU2dO0YEu3pW_3pJ7U66buCmZZA=;";
-    private static final String meta_ = "eyJhZ2VudCI6eyJwcm9ncmFtIjoiZ3Jhc3Nob3BwZXIiLCJwcm9ncmFtVmVyc2lvbiI6IllXNTVJQ3NnYjJ4a0lDWWdaR0YwWVE9PSIsInBsdWdpblZlcnNpb24iOiIxLjIuMyIsInBsYXRmb3JtIjoib3N4LzEwLjkifSwic291cmNlIjp7Im1haW5GaWxlIjoiTXlTY3JpcHQuZ2gifX0=";
+    private static final String meta_ = "eyJhZ2VudCI6eyJwcm9ncmFtIjoiZ3Jhc3Nob3BwZXIiLCJwcm9ncmFtVmVyc2lvbiI6IllXNTVJQ3NnYjJ4a0lDWWdaR0YwWVE9PSIsInBsdWdpblZlcnNpb24iOiIxLjIuMyIsInBsYXRmb3JtIjoib3N4LzEwLjkifSwic291cmNlIjp7Im1haW5GaWxlIjoiVGFuZ28ifX0=";
     private static URL url_;
 
     static {
